b3ddc8beb149eb145c53f906222b2a6c27a8c052
Sivakkumar <kannantecht@gmail.com>
2023-12-18 19:22:38 +0530

    Integrated essential routes for handling CRUD operations on the shopping cart

---
+++ routes/cart.routes.js
@@ -0,0 +1,25 @@
+const orderController = require("../controllers/cart.controller");
+const { authJwt, requestValidator } = require("../middlewares");
+
+module.exports = function (app) {
+  //Route for the POST request to create the cart
+  app.post(
+    "/ecomm/api/v1/carts",
+    [authJwt.verifyToken],
+    orderController.create
+  );
+
+  //Route for the PUT request to update the cart
+  app.put(
+    "/ecomm/api/v1/carts/:id",
+    [authJwt.verifyToken],
+    orderController.update
+  );
+
+  //Route for the GET request to get the cart
+  app.get(
+    "/ecomm/api/v1/carts/:cartId",
+    [authJwt.verifyToken],
+    orderController.getCart
+  );
+};
