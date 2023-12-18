fc81e381e142ba7e5d5d4a7e7767fd8798ef0fa0
Sivakkumar <kannantecht@gmail.com>
2023-12-18 18:17:38 +0530

    Implemented 'Cart' schema and, established a one-to-many relationship with 'User' and a many-to-many relationship with 'Product'

---
+++ models/cart.model.js
@@ -0,0 +1,13 @@
+module.exports = (sequelize, Sequelize) => {
+  const Cart = sequelize.define("cart", {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    cost: {
+      type: Sequelize.INTEGER,
+    },
+  });
+  return Cart;
+};
