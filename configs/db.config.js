56086adfb45ba10dc239101e4619b324af5bc1d3
Sivakkumar <kannantecht@gmail.com>
2023-12-18 17:33:55 +0530

    Added dev database configuration

---
+++ configs/db.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  development: {
+    HOST: "localhost",
+    USER: "root",
+    PASSWORD: "596$?KFn#3lwM",
+    DB: "ecom_db",
+    dialect: "mysql",
+    pool: {
+      max: 5,
+      min: 0,
+      acuire: 30000, //max time in ms that a pool will try to get connection before throwing error
+      idle: 10000, // maximum time in ms that a connection can be idle before being released
+    },
+  },
+};
