bcff0e19c5fabcaaa36727e47064b0171d51d84a
Sivakkumar <kannantecht@gmail.com>
2023-12-18 19:59:07 +0530

    Revised the initial test script to use 'jest' exclusively, and implemented unit tests for category CRUD operations with mock data. Configured a test database for the environment

--- configs/db.config.js
+++ configs/db.config.js
@@ -12,4 +12,17 @@ module.exports = {
       idle: 10000, // maximum time in ms that a connection can be idle before being released
     },
   },
+  test: {
+    HOST: "localhost",
+    USER: "root",
+    PASSWORD: "596$?KFn#3lwM",
+    DB: "ecom_test_db",
+    dialect: "mysql",
+    pool: {
+      max: 5,
+      min: 0,
+      acuire: 30000, //max time in ms that a pool will try to get connection before throwing error
+      idle: 10000, // maximum time in ms that a connection can be idle before being released
+    },
+  },
 };
